c8543339e43df58763cea1fd7ddb4a4f61962a36
mamunuddinbu <mamun.bbn.bd@gmail.com>
2023-04-30 12:55:47 +0600

    news dragon project updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,17 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyCoZeuG8a8rPB_wpu1B7PCcJZwbgHnr1Vc",
+  authDomain: "news-dragon-project-1a4d3.firebaseapp.com",
+  projectId: "news-dragon-project-1a4d3",
+  storageBucket: "news-dragon-project-1a4d3.appspot.com",
+  messagingSenderId: "867727377238",
+  appId: "1:867727377238:web:230bb623e5cde3cbefccf7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export default app;
